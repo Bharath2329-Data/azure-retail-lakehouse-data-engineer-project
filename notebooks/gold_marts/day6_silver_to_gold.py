22e057b6dc32d6c7074dba3d65c2664c3aeec15e
Bharath2329-Data <bruyyadi@gmail.com>
2026-03-07 14:53:46 -0600

    day6: build gold analytics tables from silver delta data

---
+++ notebooks/gold_marts/day6_silver_to_gold.py
@@ -0,0 +1,11 @@
+## Day 6: Gold Layer Analytics
+
+Built Gold-layer business marts from Silver Delta tables using Azure Databricks.
+
+Created the following analytics tables:
+- daily_revenue
+- sales_by_store_daily
+- top_products_daily
+- customer_lifetime_value
+
+These Gold tables were written to ADLS Gen2 in Delta format and validated using row counts and schema checks. This Gold layer is designed for reporting, dashboarding, and business analysis.
